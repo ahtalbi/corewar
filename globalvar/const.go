65ebeed2ae9618603c25e16eb348cd6324e75912
abalouri <balouriabdrahman@gmail.com>
2026-08-23 00:04:15 +0100

    add

---
+++ globalvar/const.go
@@ -0,0 +1,17 @@
+package globalvar
+
+const (
+	Magic       = 0x00EA83F3
+	HeaderSize  = 2192
+	ProgNameLen = 128
+	CommentLen  = 2048
+	MemSize     = 4096
+	IdxMod      = 512
+	MaxPlayers  = 4
+	RegNumber   = 16
+	CycleToDie  = 1536
+	CycleDelta  = 5
+	NbrLive     = 21
+	MaxChecks   = 10
+	MaxExecSize = MemSize / 2
+)
